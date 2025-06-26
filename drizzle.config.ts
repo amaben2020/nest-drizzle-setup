8de474bd0731ad1dbe1701cf2df4390658589193
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-06-26 23:57:25 +0100

    removing hardcoded values

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,36 @@
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// console.log(process.env);
+
+// export default {
+//   schema: './src/database/schema.ts',
+//   out: './drizzle/migrations',
+//   dialect: 'mysql',
+//   dbCredentials: {
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//   },
+// };
+
+// drizzle.config.ts
 import * as dotenv from 'dotenv';
-dotenv.config();
+
+const ENV = process.env.NODE_ENV ?? 'development';
+dotenv.config({ path: `.env.${ENV}` }); // 🔥 This ensures the correct env file is loaded
 
 export default {
   schema: './src/database/schema.ts',
   out: './drizzle/migrations',
   dialect: 'mysql',
   dbCredentials: {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
   },
 };
