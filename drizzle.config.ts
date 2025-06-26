22e50efbdac54cb5ea5e8d650a276221f54fb03c
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-06-27 00:24:00 +0100

    adding new row

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,22 +1,3 @@
-// import * as dotenv from 'dotenv';
-// dotenv.config();
-
-// console.log(process.env);
-
-// export default {
-//   schema: './src/database/schema.ts',
-//   out: './drizzle/migrations',
-//   dialect: 'mysql',
-//   dbCredentials: {
-//     host: process.env.DB_HOST,
-//     port: Number(process.env.DB_PORT),
-//     user: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//   },
-// };
-
-// drizzle.config.ts
 import * as dotenv from 'dotenv';
 
 const ENV = process.env.NODE_ENV ?? 'development';
