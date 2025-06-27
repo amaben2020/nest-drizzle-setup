d111667c394b5782362890b25244b53a3890d592
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-06-27 10:12:54 +0100

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,47 @@
+// import * as dotenv from 'dotenv';
+
+// const ENV = process.env.NODE_ENV ?? 'development';
+// dotenv.config({ path: `.env.${ENV}` }); // 🔥 This ensures the correct env file is loaded
+
+// export default {
+//   schema: './src/database/schema.ts',
+//   out: './drizzle/migrations',
+//   dialect: 'mysql',
+//   dbCredentials: {
+//     host: process.env.DB_HOST!,
+//     port: Number(process.env.DB_PORT),
+//     user: process.env.DB_USERNAME!,
+//     password: process.env.DB_PASSWORD!,
+//     database: process.env.DB_DATABASE!,
+//   },
+// };
+
+import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 
 const ENV = process.env.NODE_ENV ?? 'development';
-dotenv.config({ path: `.env.${ENV}` }); // 🔥 This ensures the correct env file is loaded
+const envFile = `.env.${ENV}`;
+
+// Try to load .env.{ENV}, else fallback to .env (for GitHub Actions)
+if (fs.existsSync(envFile)) {
+  dotenv.config({ path: envFile });
+  console.log(`✅ Loaded ${envFile}`);
+} else {
+  dotenv.config();
+  console.log(`⚠️  ${envFile} not found, loaded default .env instead`);
+}
 
 export default {
   schema: './src/database/schema.ts',
   out: './drizzle/migrations',
   dialect: 'mysql',
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_DATABASE!,
+    host:
+      process.env.DB_HOST! ??
+      'd1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USERNAME! ?? 'oyd3i2rs2yb26lic',
+    password: process.env.DB_PASSWORD! ?? 'mpnfrleo095gheeb',
+    database: process.env.DB_DATABASE! ?? 'wz12oseoex68yd4p',
   },
 };
